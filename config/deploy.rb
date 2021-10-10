b9daa13e061fefd99fb6360828f211f659d55b48
bananvyhe <loadonden@yaho.com>
2021-10-10 19:51:07 +0500

    fix

--- config/deploy.rb
+++ config/deploy.rb
@@ -2,7 +2,7 @@
 lock "~> 3.16.0"
 
 set :application, "voodoo"
-set :repo_url, "https://github.com/bananvyhe/voodoo.git"
+set :repo_url, "git://github.com/bananvyhe/voodoo.git"
 
 # Default branch is :master
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
