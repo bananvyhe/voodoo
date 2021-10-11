3a43a5cc966b5e5a02941ef07b122d6af951afd4
bananvyhe <loadonden@yaho.com>
2021-10-11 14:08:37 +0500

    1

--- app/javascript/packs/voodoo.js
+++ app/javascript/packs/voodoo.js
@@ -8,6 +8,7 @@
 import Vue from 'vue'
 import Vuetify from 'vuetify'
 import App from '../app.vue'
+import 'vuetify/dist/vuetify.min.css'
 Vue.use(Vuetify)
 
 document.addEventListener('DOMContentLoaded', () => {
