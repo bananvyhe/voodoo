b9834737035903995eeb0caa4a07645b7cab8b5b
bananvyhe <loadonden@yaho.com>
2021-10-11 13:55:02 +0500

    vuetify try loaders prepend

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,11 +1,12 @@
 const { environment } = require('@rails/webpacker')
 const { VueLoaderPlugin } = require('vue-loader')
 const vue = require('./loaders/vue')
-const scss = require('./loaders/scss')
+const vuetify = require('./loaders/vuetify')
  
 // options.additionalData = `@import "app/scss/variables.scss"`
 
 
 environment.plugins.prepend('VueLoaderPlugin', new VueLoaderPlugin())
 environment.loaders.prepend('vue', vue)
+environment.loaders.prepend('vuetify', vuetify)
 module.exports = environment
