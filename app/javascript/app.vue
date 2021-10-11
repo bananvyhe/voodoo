fafa1504c1ad9cd19f2be7df7d22b58ebefa4d55
bananvyhe <loadonden@yaho.com>
2021-10-11 15:37:52 +0500

    all works

--- app/javascript/app.vue
+++ app/javascript/app.vue
@@ -1,6 +1,12 @@
 <template>
   <div id="app">
-    <p>{{ message }}</p>
+    <v-app>
+      <v-container>
+        <v-main>
+          <p>{{ message }}</p>          
+        </v-main>
+      </v-container>
+    </v-app>
   </div>
 </template>
 
@@ -8,15 +14,19 @@
 export default {
   data: function () {
     return {
-      message: "Hello Voodoo!"
+      message: "Voodoo invition"
     }
   }
 }
 </script>
 
 <style scoped>
+#app{
+    background-color: #1a2b08;
+  }
 p {
-  font-size: 2em;
+  font-size: 1em;
   text-align: center;
+  color: #999;
 }
 </style>
