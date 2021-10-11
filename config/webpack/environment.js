ccd7fbdf255d87bc971970519418021002f6f657
bananvyhe <loadonden@yaho.com>
2021-10-11 13:44:40 +0500

    1

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -2,16 +2,11 @@ const { environment } = require('@rails/webpacker')
 const { VueLoaderPlugin } = require('vue-loader')
 const vue = require('./loaders/vue')
 const scss = require('./loaders/scss')
-
-const sassLoader = environment.loaders.get('sass')
-const sassLoaderConfig = sassLoader.use.find(function(element) {
-  return element.loader == 'sass-loader'
-})
-const options = sassLoaderConfig.options
-options.implementation = require('sass')
+ 
 // options.additionalData = `@import "app/scss/variables.scss"`
 
 
 environment.plugins.prepend('VueLoaderPlugin', new VueLoaderPlugin())
 environment.loaders.prepend('vue', vue)
+environment.loaders.prepend('scss', scss)
 module.exports = environment
