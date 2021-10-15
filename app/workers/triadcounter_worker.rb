eff4da5836e50036b3d82f1c0189738d9ca3b5f3
bananvyhe <loadonden@yahoo.com>
2021-10-15 20:59:20 +0500

    send params to news controller

--- app/workers/triadcounter_worker.rb
+++ app/workers/triadcounter_worker.rb
@@ -4,21 +4,23 @@ class TriadcounterWorker < ApplicationController
 	require 'mechanize'
 	require 'json'
 	require 'httparty'  
-
- 
+	@rowsd = Array.new
 	def perform
 		def selection_scrapped(row, url)
 			title  = row.css('.list_article_photo img').attr('src').to_s
-			titlecrop = title.slice(0, title.rindex('/dims'))
-			news_link = row.css('.list_article_headline a').attr('href')
+			titlecrop = title.slice(0, title.rindex('/dims')).to_s
+			news_link = row.css('.list_article_headline a').attr('href').to_s
 			head = row.css('.list_article_headline a').inner_text.to_s
-			date = row.css('.list_article_byline2').inner_text
-			article = row.css('.list_article_lead a').inner_text
-			puts titlecrop
-			puts news_link
-			puts head 
-			puts article
-			puts date
+			date = row.css('.list_article_byline2').inner_text.to_s
+			article = row.css('.list_article_lead a').inner_text.to_s
+			@rowsd = {
+		    :imglink => titlecrop,
+		    :head => head,
+		    :content => article,
+		    :date => date,
+		    :link => news_link
+		  }	
+	 
 		end
 
 		agent = Mechanize.new
@@ -32,6 +34,15 @@ class TriadcounterWorker < ApplicationController
 		page.css('.list_article_area').each do |row|
 			selection_scrapped(row, url)
 		end
+		headers = {
+			"Content-Type" => "application/json"  
+		}
+		if Rails.env.production?
+			HTTParty.post("https://voodoo.ru/news",headers: headers, body: @rowsd.to_json)
+		end
+		if Rails.env.development?
+			HTTParty.post("http://localhost:3000/news",headers: headers, body: @rowsd.to_json)
+		end		
 	end
 
 end
